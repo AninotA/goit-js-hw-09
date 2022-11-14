509bff143cc7e4229e80a436dd38a6f98865ebd1
Antonina Buchak <antoninabucak@gmail.com>
2022-11-14 10:36:56 +0200

    Task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  //   console.log(stopBtn);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+  startBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+});
